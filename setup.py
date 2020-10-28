d0c5a6b63dddeea61662d9f695e0d37ce3d0a67b
James Reynolds <magnusviri@me.com>
2020-10-28 16:04:22 -0600

    Updated setup project url and added paragraph to readme about the pypi move

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="Maintaining & automating Jamf Pro via the API",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/univ-of-utah-marriott-library-apple/jctl",
+    url="https://github.com/univ-of-utah-marriott-library-apple/python-jamf",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
