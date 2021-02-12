4c9d7cff4c8401a5c08ba9c098858ee29258f8bb
Tony Williams <honestpuck@gmail.com>
2021-02-12 12:44:25 +1100

    Added a package_data section to setup.py

--- setup.py
+++ setup.py
@@ -15,6 +15,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/univ-of-utah-marriott-library-apple/python-jamf",
     packages=setuptools.find_packages(),
+    package_data={'': ['*.json']},
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
