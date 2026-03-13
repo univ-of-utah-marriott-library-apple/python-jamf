436301e2845e632d1c3ba6c148bf4f84065e93a1
James Reynolds <magnsuviri@me.com>
2026-03-13 13:01:07 -0600

    Add jctl-jq

---
+++ jctl-jq.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+# example:
+#
+# jctl-jq.py computers -- general/name
+
+import sys
+import shutil
+import subprocess
+
+sep = sys.argv.index('--') if '--' in sys.argv else None
+if not sep:
+    sys.exit(f"Usage: {sys.argv[0]} <jctl args> -- field/path ...")
+
+jctl_args = sys.argv[1:sep]
+props     = sys.argv[sep+1:]
+
+if not jctl_args or not props:
+    sys.exit(f"Usage: {sys.argv[0]} <jctl args> -- field/path ...")
+
+if not shutil.which('jq'):
+    sys.exit("Error: jq not found in PATH")
+
+jctl_args += [arg for p in props for arg in ('-p', p)] + ['-j']
+
+def jq_piece(prop):
+    return '\\(.' + '.'.join(prop.split('/')) + ')'
+
+jq_filter = '.[] | "' + '\\t'.join(jq_piece(p) for p in props) + '"'
+
+print(f"Running: jctl {' '.join(jctl_args)} | jq -r '{jq_filter}'", file=sys.stderr)
+print('---', file=sys.stderr)
+
+jctl = subprocess.Popen(['jctl'] + jctl_args, stdout=subprocess.PIPE)
+jq   = subprocess.Popen(['jq', '-r', jq_filter], stdin=jctl.stdout)
+jctl.stdout.close()
+jq.wait()
